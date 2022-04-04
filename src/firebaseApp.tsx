47c30ac8336403036698af4e38a444f5e5669bcd
Zechariah Tan <zechariahtan144@gmail.com>
2022-04-04 12:03:59 +0800

    Exposed firebase config

--- src/firebaseApp.tsx
+++ src/firebaseApp.tsx
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app"
 
 export default initializeApp({
-	apiKey: import.meta.env.VITE__FIREBASE__API_KEY,
-	authDomain: import.meta.env.VITE__FIREBASE__AUTH_DOMAIN,
-	databaseURL: import.meta.env.VITE__FIREBASE__DATABASE_URL,
-	projectId: import.meta.env.VITE__FIREBASE__PROJECT_ID,
-	storageBucket: import.meta.env.VITE__FIREBASE__STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE__FIREBASE__MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE__FIREBASE__APP_ID,
-	measurementId: import.meta.env.VITE__FIREBASE__MEASUREMENT_ID
+	apiKey: "AIzaSyDJcMewiJS1CNcsuFInwLfTOkQssa3xYzc",
+	authDomain: "chatic-messenger.firebaseapp.com",
+	databaseURL: "https://chatic-messenger-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "chatic-messenger",
+	storageBucket: "chatic-messenger.appspot.com",
+	messagingSenderId: "585323220439",
+	appId: "1:585323220439:web:6c5ea3fd37e56a7b768f92",
+	measurementId: "G-QBDNK49XSD"
 })
