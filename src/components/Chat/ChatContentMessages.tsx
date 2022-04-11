b5a803291cfbc4b97d718e069aeb1029e7e10605
Zechariah <zechariahtan144@gmail.com>
2022-04-11 17:24:36 +0800

    Added scrollbar to chat list

--- src/components/Chat/ChatContentMessages.tsx
+++ src/components/Chat/ChatContentMessages.tsx
@@ -1,11 +1,34 @@
 import { AnimatePresence, motion } from "framer-motion"
 import { PropsWithChildren, useContext } from "react"
 
+import { styled } from "@mui/material"
+
 import AuthContext from "../../contexts/AuthContext"
 import useChatMessages from "../../hooks/useChatMessages"
 import useCurrentChatId from "../../hooks/useCurrentChatId"
 import MessageReceived from "./MessageReceived"
 import MessageSent from "./MessageSent"
+
+const Scrollable = styled(motion.div)(({ theme }) => ({
+	position: "absolute",
+	width: "100%",
+	height: "100%",
+	overflowX: "hidden",
+	overflowY: "scroll",
+
+	"&::-webkit-scrollbar": {
+		width: 15
+	},
+	"&::-webkit-scrollbar-track": {
+		background: "#181818"
+	},
+	"&::-webkit-scrollbar-thumb": {
+		border: "5px solid #181818",
+		background: theme.palette.primary.main,
+		backgroundClip: "padding-box",
+		borderRadius: 9999
+	}
+}))
 
 const _ChatContentMessages = (props: PropsWithChildren<{}>) => {
 	const user = useContext(AuthContext)!
@@ -21,13 +44,8 @@ const _ChatContentMessages = (props: PropsWithChildren<{}>) => {
 				overflow: "hidden"
 			}}>
 			<AnimatePresence exitBeforeEnter>
-				<motion.div
+				<Scrollable
 					key={chatId}
-					style={{
-						position: "absolute",
-						width: "100%",
-						height: "100%"
-					}}
 					transition={{ duration: 0.2 }}
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
@@ -51,7 +69,7 @@ const _ChatContentMessages = (props: PropsWithChildren<{}>) => {
 							)
 						)}
 					</AnimatePresence>
-				</motion.div>
+				</Scrollable>
 			</AnimatePresence>
 		</div>
 	)
