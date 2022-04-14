3a67a5bf9b2a13a50ee9e10b427825e81ea60f86
Zechariah <zechariahtan144@gmail.com>
2022-04-14 23:10:08 +0800

    Implemented first version of pagination

--- src/components/Chat/ChatContentMessages.tsx
+++ src/components/Chat/ChatContentMessages.tsx
@@ -1,5 +1,6 @@
 import { AnimatePresence } from "framer-motion"
-import { PropsWithChildren, useContext } from "react"
+import { PropsWithChildren, useContext, useEffect, useState } from "react"
+import { useAnimatingToEnd, useAtBottom, useAtTop } from "react-scroll-to-bottom"
 
 import AuthContext from "../../contexts/AuthContext"
 import useChatMessages from "../../hooks/useChatMessages"
@@ -15,9 +16,28 @@ const _ChatContentMessages = (
 	const { getParent } = props
 
 	const user = useContext(AuthContext)!
+	const [queryMax, setQueryMax] = useState(20)
 
+	const [atTop] = useAtTop()
+	const [atBottom] = useAtBottom()
+	const [animatingToEnd] = useAnimatingToEnd()
 	const chatId = useCurrentChatId()!
-	const messages = useChatMessages(chatId, 50)
+	const messages = useChatMessages(chatId, queryMax)
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setQueryMax(queryMax => queryMax + 20)
+		}, 500)
+		return () => clearTimeout(timeout)
+	}, [])
+
+	useEffect(() => {
+		const parent = getParent()
+		if (parent && atTop && !atBottom && !animatingToEnd) {
+			setQueryMax(queryMax => queryMax + 20)
+			parent.scrollTo({ top: 1, behavior: "smooth" })
+		}
+	}, [atTop, atBottom, animatingToEnd])
 
 	const parent = getParent()
 	if (parent && parent.scrollTop < 1) {
@@ -26,7 +46,6 @@ const _ChatContentMessages = (
 
 	return (
 		<AnimatePresence>
-			<div key="header" style={{ height: 12 }} />
 			{messages?.map(message =>
 				message.userId === user.id ? (
 					<MessageSent key={message.id} message={message} />
@@ -34,7 +53,6 @@ const _ChatContentMessages = (
 					<MessageReceived key={message.id} message={message} />
 				)
 			)}
-			<div key="footer" style={{ height: 12 }} />
 		</AnimatePresence>
 	)
 }
