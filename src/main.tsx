f2dbb4b9d9c966253b36dd223a518e0fc8959a19
Zechariah <zechariahtan144@gmail.com>
2022-04-17 19:46:59 +0800

    Added default snackbar props

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { SnackbarProvider } from "notistack"
 import { StrictMode } from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 
 import App from "./App"
+import SnackbarProvider from "./components/SnackbarProvider"
 import { AuthProvider } from "./contexts/AuthContext"
 
 ReactDOM.render(
 	<StrictMode>
 		<BrowserRouter>
 			<AuthProvider>
-				<SnackbarProvider maxSnack={5}>
+				<SnackbarProvider>
 					<App />
 				</SnackbarProvider>
 			</AuthProvider>
