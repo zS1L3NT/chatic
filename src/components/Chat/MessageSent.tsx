ded9a26a7819f429659c55e6f3e45283e9aa5fa5
Zechariah Tan <zechariahtan144@gmail.com>
2022-04-20 16:58:51 +0800

    Moved context menus in messages to their own files

--- src/components/Chat/MessageSent.tsx
+++ src/components/Chat/MessageSent.tsx
@@ -1,18 +1,11 @@
 import { motion } from "framer-motion"
 import { DateTime } from "luxon"
-import { useSnackbar } from "notistack"
 import { PropsWithChildren, useState } from "react"
 
-import { ContentCopy, Delete, Edit, Reply } from "@mui/icons-material"
-import {
-	Card, Divider, ListItemIcon, ListItemText, Menu, MenuItem, MenuList, Tooltip
-} from "@mui/material"
+import { Card } from "@mui/material"
 
-import useAppDispatch from "../../hooks/useAppDispatch"
-import useCurrentChatId from "../../hooks/useCurrentChatId"
-import { set_input } from "../../slices/InputsSlice"
 import Dot from "../Dot"
-import DeleteMessageDialog from "../Popups/DeleteMessageDialog"
+import MessageSentMenu from "../Popups/MessageSentMenu"
 
 const _MessageSent = (
 	props: PropsWithChildren<{
@@ -23,74 +16,12 @@ const _MessageSent = (
 	const { message, editable } = props
 
 	const [contextMenu, setContextMenu] = useState<{ left: number; top: number }>()
-	const [deleteMessageDialogOpen, setDeleteMessageDialogOpen] = useState(false)
-
-	const { enqueueSnackbar } = useSnackbar()
-	const chatId = useCurrentChatId()
-
-	const dispatch = useAppDispatch()
 
 	const handleOpen = (event: React.MouseEvent) => {
 		event.preventDefault()
 		setContextMenu(
 			!contextMenu ? { left: event.clientX - 2, top: event.clientY - 4 } : undefined
 		)
-	}
-
-	const handleClose = () => {
-		setContextMenu(undefined)
-	}
-
-	const handleClickReply = () => {
-		if (!chatId) return
-
-		setContextMenu(undefined)
-		dispatch(
-			set_input({
-				chatId,
-				text: "",
-				type: "reply",
-				messageId: message.id
-			})
-		)
-	}
-
-	const handleClickEdit = () => {
-		if (!chatId) return
-
-		setContextMenu(undefined)
-		dispatch(
-			set_input({
-				chatId,
-				text: message.content!,
-				type: "edit",
-				messageId: message.id
-			})
-		)
-	}
-
-	const handleClickDelete = () => {
-		if (!chatId) return
-
-		setContextMenu(undefined)
-		setDeleteMessageDialogOpen(true)
-	}
-
-	const handleClickCopyText = async () => {
-		if (!chatId) return
-
-		setContextMenu(undefined)
-		if ("clipboard" in navigator) {
-			try {
-				await navigator.clipboard.writeText(message.content)
-				enqueueSnackbar("Copied text to clipboard", { variant: "success" })
-			} catch (err) {
-				console.error(err)
-				enqueueSnackbar("Error copying text to clipboard", { variant: "error" })
-			}
-		} else {
-			enqueueSnackbar("Clipboard unavailable on your browser", { variant: "error" })
-		}
 	}
 
 	const getColor = () => {
@@ -105,15 +36,6 @@ const _MessageSent = (
 				return "rgb(0, 255, 0)"
 		}
 	}
-
-	const EditableWrapper = (props: PropsWithChildren<{ title: string }>) =>
-		editable ? (
-			<>{props.children}</>
-		) : (
-			<Tooltip title={props.title} placement="left" arrow>
-				{props.children as JSX.Element}
-			</Tooltip>
-		)
 
 	return (
 		<>
@@ -158,58 +80,12 @@ const _MessageSent = (
 						/>
 					</div>
 				</Card>
-				<Menu
-					PaperProps={{ style: { width: 200 } }}
-					BackdropProps={{
-						onContextMenu: e => e.preventDefault()
-					}}
-					open={!!contextMenu}
-					onClose={handleClose}
-					anchorReference="anchorPosition"
-					anchorPosition={contextMenu}>
-					<MenuList>
-						<MenuItem onClick={handleClickReply}>
-							<ListItemIcon>
-								<Reply fontSize="small" />
-							</ListItemIcon>
-							<ListItemText>Reply</ListItemText>
-						</MenuItem>
-						<EditableWrapper title="Message is too old to be edited!">
-							<MenuItem
-								sx={{ pointerEvents: "auto!important" }}
-								disabled={!editable}
-								onClick={handleClickEdit}>
-								<ListItemIcon>
-									<Edit fontSize="small" />
-								</ListItemIcon>
-								<ListItemText>Edit</ListItemText>
-							</MenuItem>
-						</EditableWrapper>
-						<EditableWrapper title="Message is too old to be deleted!">
-							<MenuItem
-								sx={{ pointerEvents: "auto!important" }}
-								disabled={!editable}
-								onClick={handleClickDelete}>
-								<ListItemIcon>
-									<Delete fontSize="small" />
-								</ListItemIcon>
-								<ListItemText>Delete</ListItemText>
-							</MenuItem>
-						</EditableWrapper>
-						<Divider />
-						<MenuItem onClick={handleClickCopyText}>
-							<ListItemIcon>
-								<ContentCopy fontSize="small" />
-							</ListItemIcon>
-							<ListItemText>Copy Text</ListItemText>
-						</MenuItem>
-					</MenuList>
-				</Menu>
 			</motion.div>
-			<DeleteMessageDialog
-				messageId={message.id}
-				open={deleteMessageDialogOpen}
-				setOpen={setDeleteMessageDialogOpen}
+			<MessageSentMenu
+				message={message}
+				editable={editable}
+				contextMenu={contextMenu}
+				setContextMenu={setContextMenu}
 			/>
 		</>
 	)
